a3a9cc4c9172726e35c9bea2b6435b217f5f4e8c
Elias Mendel <efm_git@conceptualize.dev>
2024-11-20 22:56:51 +0800

    Updated Hero and Carousel

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from projects.models import Project
 
 # Create your views here.
 # pages/views.py
 
-from django.shortcuts import render
-
 def home(request):
-    return render(request, "pages/home.html", {})
+    projects = Project.objects.all()
+    context = {
+        "projects": projects
+    }
+    return render(request, "pages/home.html", context)
--- projects/models.py
+++ projects/models.py
@@ -5,3 +5,8 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
+    link = models.URLField(blank=True, null=True)
+    github_link = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
