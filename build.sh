4cc3e384c06f0e88eb987cfd7e1957caad0f3c77
Elias Mendel <efm_git@conceptualize.dev>
2024-11-21 17:45:18 +0800

    UPDATE uv install

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env.sh
 uv sync --no-dev --locked
 
 # Convert static asset files
